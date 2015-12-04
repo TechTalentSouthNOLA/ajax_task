b32b1999f3f3138af88599e595517494c5885403
Sia <siakaramalegos@gmail.com>
2015-12-04 10:26:30 -0600

    Added users and login with facebook

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+
+  # Create user using the hash that's returned from the Facebook API
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
+
+  # Pulls over the current facebook profile avatar
+  def small_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=small"
+  end
+
+  # Pulls over the current facebook profile normal sized image
+  def normal_image
+    "http://graph.facebook.com/#{self.uid}/picture?type=normal"
+  end
+end
